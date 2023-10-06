b98b13d0265a7bc01831de87d9035a35535e4ca0
guidorc <guido.rc98@gmail.com>
2023-10-06 15:30:01 -0300

    Body parsing + disable access control

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // import defined routes
 const productRoutes = require('./api/routes/products');
@@ -9,9 +10,24 @@ const orderRoutes = require('./api/routes/orders');
 // use morgan to log incoming requests
 app.use(morgan('dev'));
 
+// setup body parsing
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // forward requests to specific routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// Allow access from any client (prevent CORS errors)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        // tell browser which methods are available
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({});
+    }
+});
 
 // handle request to non specified routes
 app.use((req, res, next) => {
