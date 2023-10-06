a0ac9e7e607f28d4481119a6779a695746d929c0
guidorc <guido.rc98@gmail.com>
2023-10-06 15:06:11 -0300

    Error handling + improved setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-ecommerce
 Node.Js restful API for ecommerce platform
+
+### Setup and execute
+- To set up the project, run npm install
+- to start the server, execute the command ``npm start`` in the project directory.
--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+// import defined routes
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// use morgan to log incoming requests
+app.use(morgan('dev'));
+
+// forward requests to specific routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// handle request to non specified routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// hande all errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
 
 module.exports = app;
