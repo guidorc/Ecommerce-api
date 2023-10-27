fe6f7ac28ab100f58d68f2a2086098aeb37f9be1
guidorc <guido.rc98@gmail.com>
2023-10-27 13:38:43 -0300

    Convert orders controller to async/await

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -1,123 +1,123 @@
 // import models
 const Order = require("../models/order");
 const Product = require("../models/product");
+const mongoose = require("mongoose");
 
-exports.get_all = (req, res, next) => {
-  Order.find()
-    .select("product quantity _id")
-    .populate("product", "name")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        orders: docs.map((doc) => {
-          return {
-            ...doc.toObject(),
-            info: {
-              type: "GET",
-              description: "Get order by id",
-              url: "http://localhost:3000/orders/" + doc._id,
-            },
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Failed to find orders",
-        error: err,
-      });
-    });
-};
+exports.get_all = async (req, res, next) => {
+  try {
+    const docs = await Order.find()
+      .select("product quantity _id")
+      .populate("product", "name")
+      .exec();
 
-exports.create = (req, res, next) => {
-  // verify product exists in db
-  Product.findById(req.body.productId)
-    .then((product) => {
-      if (!product) {
-        return res.status(404).json({
-          message: "Product not found",
-        });
-      }
-      // create order db object
-      const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId,
-      });
-      // save order to db
-      return order.save();
-    })
-    .then((result) => {
-      res.status(201).json({
-        message: "Order created successfully",
-        createdOrder: {
-          _id: result.id,
-          productId: result.product,
-          quantity: result.quantity,
-          info: {
-            method: "GET",
-            description: "Get product by id",
-            url: "http://localhost:3000/products/" + result.product,
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Failed to create order",
-        error: err,
-      });
-    });
-};
-
-exports.get_by_id = (req, res, next) => {
-  const id = req.params.orderId;
-  Order.findById(id)
-    .select("product quantity _id")
-    .populate("product")
-    .exec()
-    .then((result) => {
-      if (result) {
-        res.status(200).json({
-          ...result.toObject(),
+    const orders = {
+      count: docs.length,
+      orders: docs.map((doc) => {
+        return {
+          ...doc.toObject(),
           info: {
             type: "GET",
-            description: "Get all orders",
-            url: "http://localhost:3000/orders",
+            description: "Get order by id",
+            url: "http://localhost:3000/orders/" + doc._id,
           },
-        });
-      } else {
-        res.status(404).json({
-          message: "Unable to find requested order",
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
+        };
+      }),
+    };
+
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to find orders",
+      error: err,
     });
+  }
 };
 
-exports.delete = (req, res, next) => {
-  const id = req.params.orderId;
-  Order.deleteOne({ _id: id })
-    .exec()
-    .then(() => {
+exports.create = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.body.productId);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    // create order db object
+    const order = new Order({
+      _id: new mongoose.Types.ObjectId(),
+      quantity: req.body.quantity,
+      product: req.body.productId,
+    });
+
+    // save order to db
+    const result = await order.save();
+
+    return res.status(201).json({
+      message: "Order created successfully",
+      createdOrder: {
+        _id: result.id,
+        productId: result.product,
+        quantity: result.quantity,
+        info: {
+          method: "GET",
+          description: "Get product by id",
+          url: "http://localhost:3000/products/" + result.product,
+        },
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to create order",
+      error: err,
+    });
+  }
+};
+
+exports.get_by_id = async (req, res, next) => {
+  try {
+    const id = req.params.orderId;
+    const order = await Order.findById(id)
+      .select("product quantity _id")
+      .populate("product")
+      .exec();
+
+    if (order) {
       res.status(200).json({
-        message: "Order deleted successfully",
+        ...order.toObject(),
         info: {
           type: "GET",
           description: "Get all orders",
           url: "http://localhost:3000/orders",
         },
       });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
+    } else {
+      res.status(404).json({
+        message: "Unable to find requested order",
       });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: err,
     });
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    const id = req.params.orderId;
+    await Order.deleteOne({ _id: id }).exec();
+    res.status(200).json({
+      message: "Order deleted successfully",
+      info: {
+        type: "GET",
+        description: "Get all orders",
+        url: "http://localhost:3000/orders",
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
 };
--- test/controllers/ordersController.test.js
+++ test/controllers/ordersController.test.js
@@ -4,65 +4,65 @@ const orderController = require("../../api/controllers/orders");
 const Order = require("../../api/models/order");
 
 describe("Order Controller", () => {
-  let mockOrders;
-  let findStub = sinon.stub(Order, "find");
+  let res;
+  let findStub;
 
-  describe.only("Get all orders", () => {
-    describe("When orders are found successfully", () => {
-      const req = {};
-      const res = {
-        status: sinon.stub().returnsThis(),
-        json: sinon.spy(),
-      };
-
-      beforeEach(async () => {
-        mockOrders = [{ _id: "orderId", product: "productId", quantity: 2 }];
-        findStub.returns({
-          select: sinon.stub().returns({
-            populate: sinon
-              .stub()
-              .returns({ exec: sinon.stub().resolves(mockOrders) }),
-          }),
-        });
-
-        return orderController.get_all(req, res);
-      });
-
-      it("should return a 200 response with all orders", function () {
-        expect(res.status.calledWith(200)).to.be.true;
-        expect(res.json.calledOnce).to.be.true;
-        expect(res.json.firstCall.args[0].orders.length).to.equal(
-          mockOrders.length
-        );
-      });
-    });
-  });
-
-  it("should send a 500 error response on failure", (done) => {
-    const findStub = sinon.stub(Order, "find");
-    findStub.returns({
-      select: sinon.stub().returns({
-        populate: sinon
-          .stub()
-          .returns({ exec: sinon.stub().rejects("Error message") }),
+  const mockOrders = [
+    {
+      toObject: () => ({
+        _id: "orderId",
+        product: "productId",
+        quantity: 2,
       }),
-    });
+    },
+  ];
 
-    const req = {};
-    const res = {
+  beforeEach(() => {
+    res = {
       status: sinon.stub().returnsThis(),
       json: sinon.spy(),
     };
+  });
 
-    orderController.get_all(req, res);
-
+  afterEach(() => {
     findStub.restore();
-    expect(res.status.calledWith(500)).to.be.true;
-    expect(res.json.calledOnce).to.be.true;
-    expect(res.json.firstCall.args[0].message).to.equal(
-      "Failed to find orders"
-    );
+  });
 
-    done();
+  describe("Get all orders", () => {
+    it("should send a 200 response with all orders on success", async () => {
+      const execStub = sinon.stub();
+      const populateStub = sinon.stub().returns({ exec: execStub });
+      const selectStub = sinon.stub().returns({ populate: populateStub });
+
+      execStub.resolves(mockOrders);
+
+      findStub = sinon.stub(Order, "find").returns({ select: selectStub });
+
+      await orderController.get_all({}, res);
+
+      expect(res.status.calledWith(200)).to.be.true;
+      expect(res.json.calledOnce).to.be.true;
+      expect(res.json.firstCall.args[0].orders.length).to.equal(
+        mockOrders.length
+      );
+    });
+
+    it("should send a 500 error response on failure", async () => {
+      const execStub = sinon.stub();
+      const populateStub = sinon.stub().returns({ exec: execStub });
+      const selectStub = sinon.stub().returns({ populate: populateStub });
+
+      execStub.rejects("Error message");
+
+      findStub = sinon.stub(Order, "find").returns({ select: selectStub });
+
+      await orderController.get_all({}, res);
+
+      expect(res.status.calledWith(500)).to.be.true;
+      expect(res.json.calledOnce).to.be.true;
+      expect(res.json.firstCall.args[0].message).to.equal(
+        "Failed to find orders"
+      );
+    });
   });
 });
